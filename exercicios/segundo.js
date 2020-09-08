a8678acc03acaa9b5a36beaa3f8daa55e8c1213c
ernesto <ernestobayma@gmail.com>
2020-09-08 14:51:49 -0300

    initial commit

---
+++ exercicios/segundo.js
@@ -0,0 +1,26 @@
+const checkIfTriangle = (a = 1, b = 1, c = 1) =>
+  a + b > c && a + c > b && c + b > a;
+const whichType = function (a, b, c) {
+  if (a === b && a === c) {
+    return "equilateral";
+  } else if ((a === b && a !== c) || (a === c && a !== b)) {
+    return "isosceles";
+  }
+  return "scalene";
+};
+
+const triangleClassification = function (a = 1, b = 1, c = 1) {
+  if (checkIfTriangle(a, b, c)) {
+    const type = whichType(a, b, c);
+    console.log(`this is a ${type} triangle`);
+  } else {
+    console.log(
+      `Is not possible to form a triangle from the values ${a}, ${b}, ${c}`
+    );
+  }
+};
+
+triangleClassification(10, 10 , 10);
+triangleClassification(1, 2, 3);
+triangleClassification(10, 20 ,30);
+triangleClassification
