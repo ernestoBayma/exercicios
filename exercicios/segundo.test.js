ea0e2ca858251eb846e605ec3764eb3664d23025
ernesto <ernestobayma@gmail.com>
2020-09-08 15:30:06 -0300

    Added second exercise and a test

---
+++ exercicios/segundo.test.js
@@ -0,0 +1,17 @@
+const triangleClassification = require('./segundo');
+
+test('equilateral triangle', () => {
+    expect(triangleClassification(10, 10, 10)).toBe('equilateral')
+})
+
+test('scalene triangle', () => {
+    expect(triangleClassification(15, 20, 30)).toBe('scalene')
+})
+
+test('isosceles triangle', () => {
+    expect(triangleClassification(10, 10, 15)).toBe('isosceles')
+})
+
+test('not a valid triangle', () => {
+    expect(triangleClassification(1, 2, 3)).toBe('invalid')
+})
