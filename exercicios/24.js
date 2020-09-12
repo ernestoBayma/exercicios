8101bf4cd962397c210eb453499b3ce624045b18
ernesto <ernestobayma@gmail.com>
2020-09-11 22:49:19 -0300

    added 24.js and fixed 23.js

---
+++ exercicios/24.js
@@ -0,0 +1,16 @@
+/*
+    Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while
+*/
+
+
+const helloWorld = () =>  {
+    let counter = 11; 
+    while(counter > 0) {
+        console.log(" Hello World!");
+        counter--;
+    }
+
+}
+
+
+helloWorld();
